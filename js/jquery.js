3dbe2648f14313e06af2c934f6e31f2073d11e55
Gus <gustavo.schiavon@gmail.com>
2020-10-15 10:22:15 -0300

    adjust Jquery script

--- js/jquery.js
+++ js/jquery.js
@@ -6,7 +6,6 @@ $(document).ready(function(){
 
     $('#mobile-menu').toggleClass('hide');
 
-  console.log('Burger clicked')
   });
 
   $('.mobile-menu a').click(function(){
@@ -24,7 +23,6 @@ $(document).ready(function(){
     $('.row').each(function() {
       if (text > image) {
         imageHeight = $('.img-container').height(textHeight);
-        console.log('adjusted')
       } 
     });
   }
@@ -33,92 +31,8 @@ $(document).ready(function(){
   adjustHeight(textHeight, imageHeight)
 });
 
-// Google Maps Dark Mode
-
-function initMap() {
-  // Styles a map in night mode.
-  const map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: 40.674, lng: -73.945 },
-    zoom: 12,
-    styles: [
-      { elementType: "geometry", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.stroke", stylers: [{ color: "#242f3e" }] },
-      { elementType: "labels.text.fill", stylers: [{ color: "#746855" }] },
-      {
-        featureType: "administrative.locality",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "geometry",
-        stylers: [{ color: "#263c3f" }],
-      },
-      {
-        featureType: "poi.park",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#6b9a76" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry",
-        stylers: [{ color: "#38414e" }],
-      },
-      {
-        featureType: "road",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#212a37" }],
-      },
-      {
-        featureType: "road",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#9ca5b3" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry",
-        stylers: [{ color: "#746855" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "geometry.stroke",
-        stylers: [{ color: "#1f2835" }],
-      },
-      {
-        featureType: "road.highway",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#f3d19c" }],
-      },
-      {
-        featureType: "transit",
-        elementType: "geometry",
-        stylers: [{ color: "#2f3948" }],
-      },
-      {
-        featureType: "transit.station",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#d59563" }],
-      },
-      {
-        featureType: "water",
-        elementType: "geometry",
-        stylers: [{ color: "#17263c" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.fill",
-        stylers: [{ color: "#515c6d" }],
-      },
-      {
-        featureType: "water",
-        elementType: "labels.text.stroke",
-        stylers: [{ color: "#17263c" }],
-      },
-    ],
-  });
+// Container size calculation
+const imageContainer = document.getElementsByClassName('responsive-img-container')
+const calcContainerSize = function(imageContainer) {
+  imageContainer.each(height())
 }
