d5da88e97198df97829786b872b96093482deb61
Gus <gustavo.schiavon@gmail.com>
2021-05-20 16:28:44 -0300

    Update gems and images

---
+++ _site/js/jquery.js
@@ -0,0 +1,33 @@
+
+$(document).ready(function(){
+
+  $('.burger').click(function(){
+    $(this).toggleClass('open');
+
+    $('#mobile-menu').toggleClass('hide');
+
+  });
+
+  $('.mobile-menu a').click(function(){
+    $('#mobile-menu').toggleClass('hide');
+
+    $('.burger').toggleClass('open');
+
+  });  
+
+// this block allows to use 'object-fit' with Gridlex 'equalHeight' class
+  let textHeight = $('.text-container').height();
+  let imageHeight = $('.img-container').height();
+  
+  const adjustHeight = function(text, image) {
+    $('.row').each(function() {
+      if (text > image) {
+        imageHeight = $('.img-container').height(textHeight);
+      } 
+    });
+  }
+
+  
+  adjustHeight(textHeight, imageHeight)
+});
+
